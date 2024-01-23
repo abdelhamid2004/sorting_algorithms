98584a6dbd96730d9c178a46ccf2dec548ec045d
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-01-23 14:22:20 +0200

    accc

--- sort.h
+++ sort.h
@@ -18,22 +18,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* printing */
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-
-/* sorting algo */
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void swp_nd(listint_t **x, listint_t **y, listint_t *z);
-void selection_sort(int *array, size_t size);
-typedef struct listint_s
-{
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_t;
-
 /* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);

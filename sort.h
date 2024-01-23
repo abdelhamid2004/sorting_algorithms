83e3b8c973acac039dfc24ee4545de4158663836
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-01-23 14:19:20 +0200

    acc

--- sort.h
+++ sort.h
@@ -27,5 +27,29 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swp_nd(listint_t **x, listint_t **y, listint_t *z);
 void selection_sort(int *array, size_t size);
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
-#endif /* sort.h */
+/* Printing helper functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Sorting algoritms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+#endif /* SORT_H */
